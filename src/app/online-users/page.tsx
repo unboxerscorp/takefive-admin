b3862faf07a8199f6f9c4e15e4dfd840b2b77d00
Changho Park <hoya@unboxers.team>
2024-11-07 00:22:16 +0900

    feat: remove loading

--- src/app/online-users/page.tsx
+++ src/app/online-users/page.tsx
@@ -23,7 +23,7 @@ function flattenObject({ obj, result = {} }: { obj: Record<string, any>, result?
 
 export default function OnlineUsers() {
     const apiRef = useGridApiRef();
-    const [isLoading, setIsLoading] = React.useState(false);
+    // const [isLoading, setIsLoading] = React.useState(false);
     const [columns, setColumns] = React.useState<GridColDef[]>([]);
     const [rows, setRows] = React.useState<Record<string, any>[]>([]);
     const prevRowsRef = React.useRef(rows);
@@ -33,23 +33,25 @@ export default function OnlineUsers() {
     const [isVisible, setIsVisible] = React.useState(false);
 
     const fetchData = React.useCallback(() => {
-        setIsLoading(true);
         fetch(`/api/redis?${new URLSearchParams({ key: "user:*:user_data" }).toString()}`, { method: "GET" }).then(res => res.json()).catch(err => { console.error(err); return { data: {} } }).then(({ data }) => Object.values(data).map((user: any) => flattenObject({ obj: user }))).then((data) => {
             console.log(data);
             ReactDOM.flushSync(() => {
                 setCountDown(3);
-                setIsLoading(false);
+                // setIsLoading(false);
                 if (!data || data.length === 0) {
                     return;
                 }
                 setColumns(Object.keys(data[0]).map((key) => ({ field: key, headerName: key, renderCell: key === "profileImage" ? (params) => <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", height: "100%" }}><Avatar style={{ width: 40, height: 40 }} src={params.value} /></Box> : undefined, align: "center", headerAlign: "center" })));
                 setRows(data);
             });
-        }).then(() => sleep(0)).then(() =>
-            apiRef.current.autosizeColumns({
-                // includeHeaders: true,
-                includeOutliers: true,
-            }),
+        }).then(() => sleep(0)).then(() => {
+            if (!isAutoRefreshActive) {
+                apiRef.current.autosizeColumns({
+                    includeHeaders: true,
+                    includeOutliers: true,
+                })
+            }
+        }
         );
     }, [apiRef]);
 
@@ -65,6 +67,21 @@ export default function OnlineUsers() {
             console.error(`Audio permissions denied: ${e}`);
         });
     }, []);
+
+    React.useEffect(() => {
+        if (apiRef.current && isAutoRefreshActive) {
+            const interval = setInterval(() => {
+                apiRef.current.autosizeColumns({
+                    includeHeaders: true,
+                    includeOutliers: true,
+                })
+            }, 30000)
+
+            return () => {
+                clearInterval(interval);
+            }
+        }
+    }, [apiRef.current, isAutoRefreshActive]);
 
     React.useEffect(() => {
         const addSound = new Audio('/add-sound.mp3');      // 추가 효과음
@@ -145,7 +162,7 @@ export default function OnlineUsers() {
                 rows={rows}
                 columns={columns}
                 sx={{ border: 0 }}
-                loading={isLoading}
+            // loading={isLoading}
             />
         </Box>
     );
