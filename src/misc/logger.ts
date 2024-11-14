80424f2bc56c5491a5b0a7269d13d4c50a03ae22
Changho Park <hoya@unboxers.team>
2024-11-14 19:20:50 +0900

    🎨 Refactor: Simplify logger configuration by removing environment-based conditions
    - Removed conditional formatting for production and development environments, standardizing the console transport format.
    - Simplified logging format, retaining timestamp, log level, label, and message output for clarity.
    - Eliminated unnecessary `ENVIRONMENT` variable and consolidated formatting for a cleaner and more maintainable logger configuration.

--- src/misc/logger.ts
+++ src/misc/logger.ts
@@ -1,39 +1,22 @@
 import { createLogger, transports, format } from "winston";
 import { TransformableInfo } from "logform";
 
-const ENVIRONMENT = process.env.ENVIRONMENT || "dev";
-
 const getLogger = (label: string) =>
   createLogger({
-    transports: [
-      ENVIRONMENT === "prod"
-        ? new transports.Console({
-          level: "debug",
-          format: format.combine(
-            format.label({ label: `[${label}]` }),
-            format.timestamp({
-              format: "YYYY-MM-DD HH:mm:ss.SSS",
-            }),
-            format.printf(
-              (info: TransformableInfo) =>
-                `[${ENVIRONMENT}] ${info.timestamp} - ${info.level}: ${info.label} ${info.message}`
-            )
-          ),
-        })
-        : new transports.Console({
-          level: "debug",
-          format: format.combine(
-            format.label({ label: `[${label}]` }),
-            format.timestamp({
-              format: "YYYY-MM-DD HH:mm:ss.SSS",
-            }),
-            format.colorize(),
-            format.printf(
-              (info: TransformableInfo) =>
-                `[${ENVIRONMENT}] ${info.timestamp} - ${info.level}: ${info.label} ${info.message}`
-            )
-          ),
+    transports: [new transports.Console({
+      level: "debug",
+      format: format.combine(
+        format.label({ label: `[${label}]` }),
+        format.timestamp({
+          format: "YYYY-MM-DD HH:mm:ss.SSS",
         }),
+        format.colorize(),
+        format.printf(
+          (info: TransformableInfo) =>
+            `${info.timestamp} - ${info.level}: ${info.label} ${info.message}`
+        )
+      ),
+    }),
     ],
   });
 
